9b8d1349e442d05ebd283551622317c031145ca7
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-05-30 08:14:28 +0530

    Added: `study-copilot-extension/utils/tabMonitor.js`

---
+++ study-copilot-extension/utils/tabMonitor.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import axios from 'axios';
+import moment from 'moment';
+
+const TabMonitor = () => {
+  const monitorTabs = () => {
+    // Logic to monitor tabs and prompt warnings for irrelevant websites
+  };
+
+  return (
+    <div>
+      {/* JSX content for TabMonitor component */}
+    </div>
+  );
+};
+
+export default TabMonitor;
